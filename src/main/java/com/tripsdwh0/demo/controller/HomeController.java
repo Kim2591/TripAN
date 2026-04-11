7abed25bc85734346aea5103def1667f9459825b
Kimberly <kim2591025@gmail.com>
2026-04-11 12:31:15 -0600

    Agragando funcionalidades

--- src/main/java/com/tripsdwh0/demo/controller/HomeController.java
+++ src/main/java/com/tripsdwh0/demo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.tripsdwh0.demo.controller;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping
-	public String mostrarHome(Model model) {
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		List<String> lista = new LinkedList<String>();
+		lista.add("En la montaña");
+		lista.add("En la ciudad");
+		lista.add("En los pueblos");
+		lista.add("En las playas");
+		model.addAttribute("listadoTrip", lista);
 		
-		model.addAttribute("mensaje", "Hola mundo");
-		return "home";
+		return "listado2";
 	}
 
 }
